098bd17f7e00836f2be378e0d4ad0192e9d95eed
lucas ferreira <lucas.98713@gmail.com>
2020-08-06 01:06:59 -0300

    Dia 03 - Conectando Backend ao Frontend

--- aula1/web/src/components/TeacherItem/index.tsx
+++ aula1/web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,53 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const  TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnecntion() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return(
         <article className="teacher-item">
         <header>
-            <img src="https://scontent.fmgf1-2.fna.fbcdn.net/v/t31.0-8/27173330_1538427306270363_5920429175203815476_o.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_eui2=AeEtceqrHAe9yECPwN8f9TbQjdD0l19nehON0PSXX2d6EztiQNU7aO04aS31JZ3SYngMHQSCwtDVs3qBmt3lMJrz&_nc_ohc=v23m2ZCzA6AAX8O_Dhs&_nc_ht=scontent.fmgf1-2.fna&oh=b8924a260410558f5ec7b69517639815&oe=5F4D5E1F" alt="Isadora Prado"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-                <strong>Isadora Prado</strong>
-                <span>Medicina Veterinária</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
-        <p>
-            ISADORA ISADORA 
-            <br/>  <br/>
-            RABU CRISTINA
-        </p>
+        <p> {teacher.bio} </p>
         <footer>
             <p>
                 Preço/hora
-                <strong>R$ 150,00</strong>
+                <strong>R$ {teacher.cost} </strong>
             </p>
-            <button type="button">
+            <a 
+            target="_blank" 
+            onClick={createNewConnecntion} 
+            href={`https://wa.me/${teacher.whatsapp}`}
+            >
                 <img src={whatsappIcon} alt="Whatsapp"/>
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>
     );
